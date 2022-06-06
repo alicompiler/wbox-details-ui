50c786f1a5190b0e2632a28e42321fb3871150ec
Ali Faris <alihcompiler@gmail.com>
2022-06-06 21:49:56 +0300

    exports modules

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,45 @@
 import {State} from "./Data/State";
+import {fetchReducer} from "./Data/Fetch/FetchReducer";
+import {FetchActions, FetchAction, FetchActionType} from "./Data/Fetch/FetchAction";
+import {SetupAction, SetupActions, SetupActionType} from "./Data/Setup/SetupAction";
+import {DefaultsContext, defaults, Defaults, DefaultsProvider, useDefaults} from "./Defaults/DefaultsContext";
+import {Field, transformFieldValue, getFieldValue} from "./Field/Field";
+import {Group} from "./Group/Group";
+import {Provider} from "./Provider/Provider";
+import {ProviderProps} from "./Provider/ProviderProps";
+import {ServiceFactory, DefaultServiceFactory} from "./Service/ServiceFactory";
+import {BasicFetchService, BasicFetchOptions} from "./Service/Fetch/BasicFetchService";
+import {HttpFetchService, HttpFetchOptions} from "./Service/Fetch/HttpFetchService";
+import {FetchOptions} from "./Service/Fetch/Types";
 
 export type {
-    State
+    State,
+    FetchAction,
+    FetchActionType,
+    SetupAction,
+    SetupActionType,
+    Defaults,
+    Field,
+    Group,
+    ProviderProps,
+    ServiceFactory,
+    BasicFetchOptions,
+    HttpFetchOptions,
+    FetchOptions
+}
+
+export {
+    FetchActions,
+    fetchReducer,
+    SetupActions,
+    DefaultsContext,
+    DefaultsProvider,
+    useDefaults,
+    defaults,
+    transformFieldValue,
+    getFieldValue,
+    Provider,
+    DefaultServiceFactory,
+    HttpFetchService,
+    BasicFetchService
 }
